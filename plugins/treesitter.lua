99fc0f43bf0786052bcae42f34cd66d876ed16dc
Jason <jvlaj@protonmail.com>
2023-04-04 02:47:34 +1000

    ensure some lsp are installed

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "typescript", "json", "bash", "javascript", "python", "rust", "html", "go", "markdown", "yaml" },
   },
 }
